336d25178936df4e2210e02af304bf2a2998de55
Flamby <66632621+Kaeggame@users.noreply.github.com>
2024-11-25 12:02:07 +0100

    Add task componants

--- to_do_web/src/app/app.module.ts
+++ to_do_web/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TaskComponent } from './task/task.component';
+import { TaskCreateComponent } from './task-create/task-create.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TaskComponent
+    TaskComponent,
+    TaskCreateComponent
   ],
   imports: [
     BrowserModule,
